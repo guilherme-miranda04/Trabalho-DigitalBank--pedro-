4b3a35b29d6c034ab77e642dc3758956a7c1ba20
zGuilhermeTM <man.wolf07@gmail.com>
2022-11-16 21:04:22 -0300

    Update na Transferencia
    TESTE

--- src/prbancodigital/Transferencia.java
+++ src/prbancodigital/Transferencia.java
@@ -9,5 +9,20 @@ package prbancodigital;
 public class Transferencia {
     private int tipoConta;
     private String nroConta;
+    
+    public int getTipoConta() {
+        return tipoConta;
+    }
 
+    public void setTipoConta(int tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public String getNroConta() {
+        return nroConta;
+    }
+
+    public void setNroConta(String nroConta) {
+        this.nroConta = nroConta;
+    }
 }
