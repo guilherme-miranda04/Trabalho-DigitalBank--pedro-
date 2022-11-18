d96a94eb13164bf6152584c33a4859fb6b941025
zGuilhermeTM <man.wolf07@gmail.com>
2022-11-18 19:28:26 -0300

    Update Conexão Banco de Dados

--- src/prbancodigital/banco/ConexaoBanco.java
+++ src/prbancodigital/banco/ConexaoBanco.java
@@ -1,11 +1,43 @@
-
 package prbancodigital.banco;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author guilherme.miranda1
  */
-
 public class ConexaoBanco {
-    
+
+    // criar variável conexao
+    public Connection connection;
+
+    //driver, DRIVER = banco de dados, URL referente ao caminho
+    public Connection getConexao() {
+        final String DRIVER = "com.mysql.jdbc.Driver";
+        final String URL = "jdbc:mysql://localhost:3306/atendimento";
+
+        // tenta a conexao, root e root como usuário e senha   
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, "root", "root");
+            return connection;
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Driver não encontrado: " + e.toString());
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Problemas na fonte de dados." + e.toString());
+        }
+        return null;
+    }
+    //fechar conexão
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Problemas ao fechar conexão" + e.toString());
+        }
+    }
 }
