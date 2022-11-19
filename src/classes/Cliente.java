46cad2a53aaa096665736607c2100214e2e758c9
hercilio-n <hercilio.neto2003@gmail.com>
2022-11-18 21:05:44 -0300

    att classe cliente

--- src/classes/Cliente.java
+++ src/classes/Cliente.java
@@ -1,13 +1,29 @@
 package classes;
 
+import java.sql.Date;
+
 /**
  *
  * @author guilherme.miranda1
  */
 
 public class Cliente {
-    private String nome, cpf, email, dataNasc, telefone, endereco, renda;
+    private String nome, cpf, email, telefone, endereco, senha, sexo;
+    private Date dataNasc;
+    private float renda;
 
+    public Cliente(String nome, String cpf, String email, String telefone, String endereco, String senha, String sexo, Date dataNasc, float renda) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.senha = senha;
+        this.sexo = sexo;
+        this.dataNasc = dataNasc;
+        this.renda = renda;
+    }
+    
     public String getNome() {
         return nome;
     }
@@ -32,14 +48,6 @@ public class Cliente {
         this.email = email;
     }
 
-    public String getDataNasc() {
-        return dataNasc;
-    }
-
-    public void setDataNasc(String dataNasc) {
-        this.dataNasc = dataNasc;
-    }
-
     public String getTelefone() {
         return telefone;
     }
@@ -56,11 +64,40 @@ public class Cliente {
         this.endereco = endereco;
     }
 
-    public String getRenda() {
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public float getRenda() {
         return renda;
     }
 
-    public void setRenda(String renda) {
+    public void setRenda(float renda) {
         this.renda = renda;
     }
+ 
+
+   
 }
+    
+    
